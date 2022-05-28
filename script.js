d56b729e99e690ae0dd58573e5a4f45d9139d815
JK-8989 <jiyoungkim365@gmail.com>
2022-05-28 00:37:20 -0700

    Add toggle function

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ console.log(accordionItemHeader)
 for (let i = 0; i < accordionItemHeader.length; i++){
   accordionItemHeader[i].addEventListener('click', ()=>{
     accordionItemHeader[i].classList.toggle('active');
+
+    const contentBody = accordionItemHeader[i].nextElementSibling;
+
+    if(accordionItemHeader[i].classList.contains('active')){
+      contentBody.style.maxHeight = contentBody.scrollHeight + 'px';
+    } else {
+      contentBody.style.maxHeight = 0;
+    }
   });
 };
+
+
