8fee18373ed123c4062bf0f1670e3fc93b8a8e7a
JK-8989 <jiyoungkim365@gmail.com>
2022-05-28 10:50:11 -0700

    Add extra 'toggleBtn' feature to remove 'active class list'

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const accordionItemHeader = document.querySelectorAll('.accordion-item-header');
-const toggleBtn = document.querySelector('i');
+const accordionItemHeader = document.querySelectorAll(".accordion-item-header");
+const toggleBtn = document.querySelector("i");
 
-for (let i = 0; i < accordionItemHeader.length; i++){
-  accordionItemHeader[i].addEventListener('click', ()=>{
-    accordionItemHeader[i].classList.toggle('active');
+for (let i = 0; i < accordionItemHeader.length; i++) {
+  accordionItemHeader[i].addEventListener("click", () => {
+    accordionItemHeader[i].classList.toggle("active");
 
     const contentBody = accordionItemHeader[i].nextElementSibling;
 
-    if(accordionItemHeader[i].classList.contains('active')){
-      contentBody.style.maxHeight = contentBody.scrollHeight + 'px';
+    if (accordionItemHeader[i].classList.contains("active")) {
+      contentBody.style.maxHeight = contentBody.scrollHeight + "px";
     } else {
       contentBody.style.maxHeight = 0;
     }
 
-    toggleBtn.addEventListener('click', () => {
-      contentBody.style.maxHeight = 0; 
-  });
-  });
-};
+    toggleBtn.addEventListener("click", () => {
+      contentBody.style.maxHeight = 0;
 
+      accordionItemHeader[i].classList.remove("active");
 
+    });
+  });
+}
